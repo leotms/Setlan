8c663c5486b69d39369953320717043c279a097b
ALDRIX MARFIL <ALDRIX_MARFIL@HOTMAIL.COM>
2015-02-22 11:57:38 -0430

    Probando commit

---
+++ tablaSimbolos.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Creado el 22/02/2015
+Ult. Modificacion el 22/02/2015
+
+@author:  Aldrix Marfil     10-10940
+@author:  Leonardo Martinez 11-10576
+'''
