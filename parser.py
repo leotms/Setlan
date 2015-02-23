4263a5c2d5e9cc4c7202db9438d8975ca9e2dc18
ALDRIX MARFIL <ALDRIX_MARFIL@HOTMAIL.COM>
2015-02-22 21:41:25 -0430

    Arreglado caso del in

--- parser.py
+++ parser.py
@@ -65,6 +65,7 @@ def p_instruccion_scan(symbol):
 def p_instruccion_block(symbol):
     """instruccion : OPENCURLY vacio CLOSECURLY
                    | OPENCURLY lista_instrucciones CLOSECURLY
+                   | OPENCURLY declaracion vacio CLOSECURLY
                    | OPENCURLY declaracion lista_instrucciones CLOSECURLY"""
     if len(symbol) <= 4:
         symbol[0] = Block(symbol[2])
