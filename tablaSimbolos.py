fb67c8585abeca0a5e3e47914a5877ed61e2c891
leotms <martinezazuaje@gmail.com>
2015-02-25 17:43:12 -0430

    Acomodado el constructor de Simbolo y puesto los valores por defecto

--- tablaSimbolos.py
+++ tablaSimbolos.py
@@ -35,65 +35,66 @@ class Simbolo(object):
 		self.printValueIdented(string, level)
 
 class tablaSimbolos(Table):
+    pass
 
-    def __init__(self):
-        self.actual  = {}
-        self.parent  = None
-        self.errors  = []
+    # def __init__(self):
+    #     self.actual  = {}
+    #     self.parent  = None
+    #     self.errors  = []
 
-    def printTable(self, level):
-        self.printValueIdented("SCOPE\n",level)
-        for symbol in self.scope:
-            self.scope[symbol].printTable(level + 1)
-        self.printValueIdented("END_SCOPE\n",level)
-        if self.outer:
-            self.outer.printTable(level + 1)
-
-
-    def insert(self, variable, dataType):
-        if not self.contains(variable):
-            self.scope[variable] = Simbolo(variable, dataType)
-        else:
-            string = "Variable " + str(variable) + " already in scope"
-            self.error(string)
-
-    def delete(self, variable):
-        if self.contains(variable):
-            del self.scope[variable]
-        else:
-            string ="No '" + variable+ "' in scope"
-            self.error(string)
-
-    def update(self, variable, dataType, value):
-        if self.contains(variable):
-            if variable in self.scope:
-                symbol = self.scope[variable]
-
-                if dataType == symbol.dataType:
-                    symbol.value = value
-                    self.scope[variable] = symbol
-                    return True
-                else:
-                    string = "SymTable.update: Different data types"
-                    self.error.append(string)
-                    return False
-            else:
-                return self.outer.update(variable, dataType, value)
-        else:
-            print "SymTable.update: No " + variable + " in scope"
-            return False
-
-    def contains(self, variable):
-        if self.scope:
-            print(str(self.scope))
-            if variable in self.scope:
-                return True
-        else:
-            return False
-
-    def lookup(self, value):
-        pass
+    # def printTable(self, level):
+    #     self.printValueIdented("SCOPE\n",level)
+    #     for symbol in self.scope:
+    #         self.scope[symbol].printTable(level + 1)
+    #     self.printValueIdented("END_SCOPE\n",level)
+    #     if self.outer:
+    #         self.outer.printTable(level + 1)
 
 
-    def error(self, mensaje):
-        self.errors.append(mensaje)
+    # def insert(self, variable, dataType):
+    #     if not self.contains(variable):
+    #         self.scope[variable] = Simbolo(variable, dataType)
+    #     else:
+    #         string = "Variable " + str(variable) + " already in scope"
+    #         self.error(string)
+
+    # def delete(self, variable):
+    #     if self.contains(variable):
+    #         del self.scope[variable]
+    #     else:
+    #         string ="No '" + variable+ "' in scope"
+    #         self.error(string)
+
+    # def update(self, variable, dataType, value):
+    #     if self.contains(variable):
+    #         if variable in self.scope:
+    #             symbol = self.scope[variable]
+
+    #             if dataType == symbol.dataType:
+    #                 symbol.value = value
+    #                 self.scope[variable] = symbol
+    #                 return True
+    #             else:
+    #                 string = "SymTable.update: Different data types"
+    #                 self.error.append(string)
+    #                 return False
+    #         else:
+    #             return self.outer.update(variable, dataType, value)
+    #     else:
+    #         print "SymTable.update: No " + variable + " in scope"
+    #         return False
+
+    # def contains(self, variable):
+    #     if self.scope:
+    #         print(str(self.scope))
+    #         if variable in self.scope:
+    #             return True
+    #     else:
+    #         return False
+
+    # def lookup(self, value):
+    #     pass
+
+
+    # def error(self, mensaje):
+    #     self.errors.append(mensaje)
