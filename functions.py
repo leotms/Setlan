f06821cd8b93f56baa647bd97091594e40c0762b
leotms <martinezazuaje@gmail.com>
2015-03-07 20:06:30 -0430

    Agregado modulo functions.py, lo olvide en el commit anterior.

---
+++ functions.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+'''
+Creado el 07/03/2015
+Ult. Modificacion el 07/03/2015
+
+@author:  Aldrix Marfil     10-10940
+@author:  Leonardo Martinez 11-10576
+'''
+
+# Archivo que define las funciones para la evaluacion de setlan
+
+def suma(x,y):
+	return x + y
+
+def resta(x,y):
+	return x - y
+	
+def multiplicacion(x,y):
+	return x*y
+
+def division(x,y):
+	return x/y
+
